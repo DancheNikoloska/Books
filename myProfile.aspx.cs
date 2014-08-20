1f58cbc2d6dc5b1bdb8f636338f2cd48d81db0cd
Danche <danchenikoloska@gmail.com>
2014-08-20 15:48:52 +0200

    .

--- myProfile.aspx.cs
+++ myProfile.aspx.cs
@@ -11,21 +11,6 @@ public partial class myProfile : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-         
-        SqlConnection konekcija = new SqlConnection();
-        konekcija.ConnectionString = ConfigurationManager.ConnectionStrings["connection"].ConnectionString;
-        string sql = "select * from BOOKS where user_id=1";
-        SqlCommand komanda = new SqlCommand(sql, konekcija);
-        try
-        {
-            konekcija.Open();
-            SqlDataReader reader = komanda.ExecuteReader();
-            while (reader.Read())
-            {
-                mybooks.InnerHtml +="<h1>"+ reader["title"]+"</h1>";
-            }
-        }
-        catch { }
-        finally { konekcija.Close(); }
+
     }
 }
